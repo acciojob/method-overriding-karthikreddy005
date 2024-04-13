d6d9944dcd2170d08904b6008c15a1daf59a561e
karthikreddy005 <117522702+karthikreddy005@users.noreply.github.com>
2024-04-13 09:27:09 +0530

    Method Overloaded

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,26 @@
 package com.driver;
 
+class A{
+    String meth(){
+        return "Invoking method from class A";
+    }
+}
+class B extends A{
+
+}
 public class Main {
+
+    B objB = new B();
+    System.out.println(objB.meth());
+
+    class B extends A{
+        String meth(){
+
+            return "Method is overridden in Extended class B";
+
+        }
+    }
+
+    System.out.println(objB.meth());
   
 }
